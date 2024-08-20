95ad7372f6985eb9a3aea98bd19349c16ebe5ff7
Uri Baynesay <uri552@gmail.com>
2024-08-20 10:33:11 +0300

    added docker for easy installation

--- server.js
+++ server.js
@@ -11,19 +11,22 @@ const http = require('http').createServer(app)
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "docker"
+) {
+  app.use(express.static(path.resolve(__dirname, "public")))
 } else {
-    const corsOptions = {
-      origin: [
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-      ],
-      credentials: true,
-    };
-    app.use(cors(corsOptions))
+  const corsOptions = {
+    origin: [
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
 }
 
 const authRoutes = require('./api/auth/auth.routes')
